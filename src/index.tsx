87e63b9ca27d4a1ee260a67e365cdf157d5b0acd
Siddhant Gupta <guptasiddhant@icloud.com>
2020-10-18 17:18:28 +0300

    Add bitbucket

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {process.env.NODE_ENV !== "production" && (
+      <ReactQueryDevtools initialIsOpen={false} />
+    )}
   </React.StrictMode>,
   document.getElementById("root")
 );
