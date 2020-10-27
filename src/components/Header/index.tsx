09f201582ec9dbc7795418a58c2c2d0c08f2bad6
Siddhant Gupta <guptasiddhant@icloud.com>
2020-10-27 18:30:27 +0200

    Remove warning

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import {
   IconArrowLeft,
-  IconSettings,
+  // IconSettings,
   IconFilter,
   IconLogout,
 } from "tabler-icons";
